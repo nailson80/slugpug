aeec7ebe0ac743c9bc6bacb38fea8f60a5276e17
David Nelson <nailson80@gmail.com>
2016-10-14 14:16:13 -0700

    Game Loop
    Adding a very simple game loop as a starting point.

--- www/js/game.js
+++ www/js/game.js
@@ -1,3 +1,17 @@
+var SlugPug = SlugPug || {};
+
+window.main = function () {
+  window.requestAnimationFrame( main );
+    // Whatever your main loop needs to do.
+    update();
+};
+
+main(); //Start the cycle.
+
+function update() {
+    //console.log("test");
+}
+
 window.addEventListener("DOMContentLoaded", function() {
     // Setup cavas
     var canvas = document.getElementById("gameCanvas");
@@ -12,8 +26,33 @@ window.addEventListener("DOMContentLoaded", function() {
     dogSprites.src = "images/pug-sprites.png";
     dogSprites.onload = function() {
         
+    };
+    
+    SlugPug.Sprite = function(img, x, y, width, height) {
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    };
+    
+    SlugPug.drawSrite = function(context, sprite, x, y) {
+        context.drawImage(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height, x, y, sprite.width, sprite.height);
     }
     
-    context.drawImage(dogSprites,0,0,50,50,100,100,50,50);
+    var dogBody = new SlugPug.Sprite(dogSprites,0,0,50,50);
+    
+    //context.drawImage(dogSprites,dogBody.x,dogBody.y,dogBody.width,dogBody.height,100,100,50,50);
+    SlugPug.drawSrite(context, dogBody, 0, 0);
+    SlugPug.drawSrite(context, dogBody, 50, 0);
+    SlugPug.drawSrite(context, dogBody, 100, 0);
+    SlugPug.drawSrite(context, dogBody, 150, 0);
+    SlugPug.drawSrite(context, dogBody, 200, 0);
+    SlugPug.drawSrite(context, dogBody, 250, 0);
+    SlugPug.drawSrite(context, dogBody, 300, 0);
     
 });
+
+window.onkeyup = function(evt) {
+    console.log(evt);
+};
